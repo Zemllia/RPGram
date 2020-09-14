a169a035ddabd6c47c1cffe328df29214a1aff12
Nikitin Ilya <f1uctus@mail.ru>
2020-09-14 17:25:51 +0300

    Create `NonPlayable` & `Sighted` interfaces

---
+++ src/main/java/rpgram/creatures/NonPlayable.java
@@ -0,0 +1,5 @@
+package rpgram.creatures;
+
+public interface NonPlayable {
+    void makeAction();
+}
---
+++ src/main/java/rpgram/creatures/Sighted.java
@@ -0,0 +1,8 @@
+package rpgram.creatures;
+
+public interface Sighted {
+    /**
+     * Returns creature's field of vision.
+     */
+    int getFov();
+}
