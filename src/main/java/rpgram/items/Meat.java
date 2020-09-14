91cc0a7be01b5a9e4cad2a210392c64cdccd9ab8
Nikitin Ilya <f1uctus@mail.ru>
2020-09-14 17:24:56 +0300

    Add new InventoryItem: raw meat

---
+++ src/main/java/rpgram/items/Meat.java
@@ -0,0 +1,9 @@
+package rpgram.items;
+
+import com.crown.items.InventoryItem;
+
+public class Meat extends InventoryItem {
+    public Meat() {
+        super("object.nominative.rawMeat");
+    }
+}
